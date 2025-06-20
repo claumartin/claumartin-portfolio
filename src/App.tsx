ec31b5171039f170bb5cdba56d5984ce0cd2246c
claumartin <cmartin.office@gmail.com>
2025-06-20 21:34:30 +0200

    added colors
    Added defined colors as theme variables.

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,14 @@ function App() {
 
   return (
     <>
-      <div className="flex flex-col">
+      <div className="flex flex-col gap-6">
         <h1 className='text-2xl'>Hey! I'm Claudia ☀</h1>
         <h2 className='text-6xl'>Welcome to my portfolio!</h2>
-        <p className='text-2xl'>🚧 It's under construction 🚧</p>
+        <p className='text-2xl text-deep-lilac flex justify-center align-center'>
+          <span>🚧</span> 
+          <span className='mx-2 w-max'>It's under construction</span>
+          <span>🚧</span>
+        </p>
       </div>
     </>
   )
