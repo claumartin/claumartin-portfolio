06038e4556e723bedad475ac34c7adf47d91d5e7
Claudia Martín <cmartin.office@gmail.com>
2025-06-21 19:14:14 +0200

    added brief presentation (#14)

---
+++ src/components/brief-presentatio.tsx
@@ -0,0 +1,19 @@
+export const BriefPresentation: React.FC = () => {
+  return (
+    <div className="flex flex-col items-center justify-center gap-4 max-w-4xl px-8">
+      <h2 className="font-title text-center text-2xl">Brief presentation</h2>
+      <p className="text-center">
+        With more than 4 years of professional experience, I've worked with a 
+        variety of technologies, and I've been specializing more and more in the 
+        frontend area, mainly with React, TypeScript, Tailwind and Jest.
+      </p>
+      <p className="text-center">
+        I currently spend time updating myself with new tools, best practices, 
+        modern testing, and creating my own projects to regain fluency and 
+        explore new ways to transform designs into efficient, accessible and 
+        tasteful applications, writing more readable, maintainable and robust 
+        code.
+      </p>
+    </div>
+  );
+}
