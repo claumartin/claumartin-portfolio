6166df18bbbc610ab15132257441755770749a06
Claudia Martín <cmartin.office@gmail.com>
2025-06-23 16:56:38 +0200

    added technologies names (#17)

--- src/components/tech-icon.tsx
+++
@@ -1,7 +0,0 @@
-export const TechIcon: React.FC<React.PropsWithChildren> = ({children}) => {
-  return (
-    <div className="text-6xl bg-dark-purple p-2 rounded">
-      {children}
-    </div>
-  );
-}
