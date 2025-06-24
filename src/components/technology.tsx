455505b87c214a76c5317873dc7c686659188f04
Claudia Martín <cmartin.office@gmail.com>
2025-06-24 19:16:33 +0200

    improved style details (#23)

--- src/components/technology.tsx
+++ src/components/technology.tsx
@@ -5,10 +5,10 @@ interface TechnologyProps extends React.PropsWithChildren {
 export const Technology: React.FC<TechnologyProps> = ({name, children}) => {
   return (
     <div className="col-span-1 flex gap-2 items-center">
-      <div className="text-2xl bg-dark-purple p-2 rounded size-fit">
+      <div className="text-2xl p-2 rounded size-fit">
         {children}
       </div>
-      <p className="whitespace-nowrap"># {name}</p>
+      <p className="md:whitespace-nowrap"># {name}</p>
     </div> 
   );
 }
