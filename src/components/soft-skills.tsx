90603e91157582bcbc8c886811800f7b9eed7e17
claumartin <cmartin.office@gmail.com>
2025-06-26 21:42:30 +0200

    refactored soft-skills map and improved li key

--- src/components/soft-skills.tsx
+++ src/components/soft-skills.tsx
@@ -11,7 +11,7 @@ export const SoftSkills: React.FC = () => {
   <div className="flex flex-col w-fit justify-center items-center gap-4 px-8">
       <h2 className="font-title text-center text-3xl capitalize">Soft skills</h2>
       <ul className="flex flex-col gap-2 items-center text-center sm:items-start sm:text-start">
-        {softSkills?.map((softSkill) => <li key={`soft-skill-${softSkill.slice(2, 4)}`} className="md:text-nowrap">{softSkill}</li>)}
+        {softSkills?.map((softSkill) => <li key={`soft-skill-${softSkill.slice(0, 4)}`} className="md:text-nowrap">{softSkill}</li>)}
       </ul>
   </div>
   );
