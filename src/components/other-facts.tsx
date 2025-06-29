211a905a2eb299fdd89c2199bc97a737c858cc4f
claumartin <cmartin.office@gmail.com>
2025-06-29 20:47:49 +0200

    centered other-facts items on mobile

--- src/components/other-facts.tsx
+++ src/components/other-facts.tsx
@@ -5,7 +5,7 @@ export const OtherFacts: React.FC = () => {
   return (
   <div className="w-fit flex flex-col justify-center items-center gap-4 px-8 mt-6 md:mt-0">
       <h2 className="font-title text-center text-3xl capitalize">{t('about_me.title')}</h2>
-      <ul className="flex flex-col gap-2 items-start text-start list-disc">
+      <ul className="flex flex-col gap-2 items-center text-center sm:items-start sm:text-start md:list-disc">
         <li>{t('about_me.fact_1.part_1')} <em>{t('about_me.fact_1.part_2')}</em>{t('about_me.fact_1.part_3')}</li>
         <li>{t('about_me.fact_2')}</li>
         <li>{t('about_me.fact_3')}</li>
