00592ea9ce89c386a833e09604943fb91025442b
claumartin <cmartin.office@gmail.com>
2025-06-30 17:02:51 +0200

    made download cv by current language

--- src/components/download-cv.tsx
+++ src/components/download-cv.tsx
@@ -3,16 +3,17 @@ import { BiDownload } from "react-icons/bi";
 
 export const DownloadPDF: React.FC = () => {
 
-  const { t } = useTranslation();
+  const {t} = useTranslation();
+  const baseURL =  `${import.meta.env.BASE_URL}/docs/`;
 
   return (
     <a 
-      href={`${import.meta.env.BASE_URL}/docs/Claudia Martín EN.pdf`} 
-      download="Claudia Martín EN.pdf"
+      href={t('download_cv.filename', {baseURL})} 
+      download={t('download_cv.filename', {baseURL: ''})}
       className="flex gap-2 flex-nowrap items-center text-dark-pastel-purple underline"
     >
       <BiDownload />
-      {t('download_cv')}
+      {t('download_cv.title')}
     </a>
   );
 } 
